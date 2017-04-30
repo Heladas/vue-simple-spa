b521f7bd75732c46ae9d945c56317723cde0168c
Heladas <mart-workbox@yandex.ru>
2017-04-30 23:31:38 +0300

    Added simple routing by three pages

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import VueRouter from './router'
+import VueRouter from 'vue-router'
+import Hello from './components/Hello'
+import About from './components/About'
+import Contacts from './components/Contacts'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
 
-/* eslint-disable no-new */
+const routes = [
+  { path: '/', component: Hello },
+  { path: '/about', component: About },
+  { path: '/contacts', component: Contacts }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 new Vue({
   el: '#app',
-  router,
   template: '<App/>',
-  components: { App }
-})
+  components: { App, Hello, About },
+  router
+}).$mount('#app')
+
